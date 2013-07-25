525111bad9f2f05bbb537c03769f5a238b6aceff
mrshankly <mrshankly@riseup.net>
2013-07-25 16:12:23 +0100

    Added tests.

--- test/lisir_test.exs
+++ test/lisir_test.exs
@@ -1,9 +1,58 @@
 Code.require_file "test_helper.exs", __DIR__
 
 defmodule LisirTest do
-  use ExUnit.Case
+  use Lisir.Case
 
-  test "the truth" do
-    assert(true)
+  test "sum" do
+    assert lisir("(+ 1 2)") == "3"
+  end
+
+  test "sum big" do
+    assert lisir("(+ 1 2 3 4 5)") == "15"
+  end
+
+  test "division" do
+    assert lisir("(/ 6 2 1)") == "3.0"
+  end
+
+  test "comparison" do
+    assert lisir("(< 1 2 3)") == "true"
+  end
+
+  test "equal" do
+    assert lisir("(= 2 2 2)") == "true"
+  end
+
+  test "simple begin" do
+    assert lisir("(begin (+ 1 1))") == "2"
+  end
+
+  test "define" do
+    assert lisir("(begin (define x 1) x)") == "1"
+  end
+
+  test "set!" do
+    assert lisir("(begin (define x 1) (set! x 2) x)") == "2"
+  end
+
+  test "if true" do
+    assert lisir("(if (< 1 2) 1 2)") == "1"
+  end
+
+  test "if false" do
+    assert lisir("(if (= 1 2) 1 2)") == "2"
+  end
+
+  test "quote" do
+    assert lisir("(quote (+ 1 1))") == "(+ 1 1)"
+  end
+
+
+  test "lambda" do
+    assert lisir("((lambda (x) (+ x 1)) 2)") == "3"
+  end
+
+  test "define lambda" do
+    assert lisir("(begin (define square (lambda (x) (* x x))) (square 5))") == "25"
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,25 @@
 ExUnit.start
+
+defmodule Lisir.Case do
+	@moduledoc false
+
+	defmacro __using__(_) do
+		quote do
+			use ExUnit.Case, async: false
+			import unquote(__MODULE__)
+		end
+	end
+
+	@doc """
+	Starts an test repl, feeds it the provided input and returns the output.
+	"""
+	def lisir(input) do
+		ExUnit.CaptureIO.capture_io([input: input, capture_prompt: false], fn ->
+			Lisir.start end) |> strip_output
+	end
+
+	defp strip_output(s) do
+		Regex.replace(%r/\A.+?$/ms, s, "")
+		|> String.strip
+	end
+end
